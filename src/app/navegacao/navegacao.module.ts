812e5c0f9a33327ec277ab9126cd27f27e9ddba8
emanuellyogg <manuogg90@gmail.com>
2022-05-10 11:18:25 -0300

    feat: customizando a navegação

---
+++ src/app/navegacao/navegacao.module.ts
@@ -0,0 +1,28 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent
+  ]
+})
+
+export class NavegacaoModule { }
